0a6bf2879e46a29433b1f36daa9e6c993a5411e1
Marina Obruch <marina_obruch@mail.ru>
2023-11-07 20:52:26 +0500

    fix middleware

--- src/middlewares/loggerOne.js
+++ src/middlewares/loggerOne.js
@@ -1,7 +1,7 @@
 const { PORT, API_URL } = process.env;
 
 const loggerOne = (request, response, next) => {
-  console.log(`An origin url: ${API_URL}:${PORT}`);
+  console.log(`An origin url: ${request.originalUrl}`);
   console.log("Request Type:", request.method);
 
   next();
